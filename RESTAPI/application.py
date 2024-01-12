5c270c75887aa59f183d9794c2218cd3ca18d03c
Jakaria-Jihad <jakariahossen669@gmail.com>
2024-01-13 05:30:27 +0600

    Started the server successfully.

--- RESTAPI/application.py
+++ RESTAPI/application.py
@@ -8,5 +8,14 @@ def index():
 
 @app.route('/outcomes')
 def outcomes():
-    return 'There are so many goofy outcomes'
+    return 'There are so many goofy outcomes, type outcomes/details for more!'
 
+@app.route('/outcomes/details')
+def outcomes_details():
+    info = """
+    1. I will be more clear about MVC model.
+    2. Learn the Python frameworks.
+    3. Can start my journey as a developer.
+    4. Gonna get a job. hehehehe
+    """
+    return info
